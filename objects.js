f7ca71d07af1e6fb7933f1c77a72f121107fba7d
Taahira Carter <taahirac001@gmail.com>
2019-11-04 02:46:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,4 +5,7 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle 
   return playlist
+}function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
 }
