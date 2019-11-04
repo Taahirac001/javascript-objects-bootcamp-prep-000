1b5714ff4c04b0f37c111430506abe3b582a71d3
Taahira Carter <taahirac001@gmail.com>
2019-11-04 02:41:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,3 @@
-function destructivelyUpdateObjestWithKeyAndValue(obj, key, value){
-  obj[key] = value;
-  return obj
-}
-
-const recipe = {eggs: 3}
-
-function destructivelyUpdateObjestWithKeyAndValue(recipe, 'flour', '3 cups'){
+var playlist = {
   
 }
